6258c9d998e3f8a2485c09e38b61f9935ecd7832
Devesh Sharma <devesh.s785dds@gmail.com>
2023-02-28 14:17:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Booked from './components/Booked/Booked';
 import Calendar from './components/Calendar/Calendar';
 function App() {
+
+
   return (
   <Router>
     <Routes>  
+      
       <Route path="/" element={<Calendar/>}/>
+      
       <Route path="/Booked" element={<Booked/>}/>
     </Routes>
   </Router>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import App from './App';
@@ -11,6 +11,7 @@ const root = ReactDOM.createRoot(container);
 root.render(
   <StrictMode>
     <ChakraProvider theme={theme}>
+      <ColorModeScript/>
       <App />
     </ChakraProvider>
   </StrictMode>
